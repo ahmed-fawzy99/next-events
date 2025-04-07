ee2b106673350ca15bb52cfec3be2a5fdfec9fb7
Ahmed Deghady <52136140+ahmed-fawzy99@users.noreply.github.com>
2025-04-07 14:10:54 +0200

    Refactor

--- src/components/dashboard/TopSection.tsx
+++ src/components/dashboard/TopSection.tsx
@@ -15,12 +15,13 @@ export default function TopSection({myEvents}: {myEvents: Event[]}) {
                           className="absolute inset-0 p-4 overflow-y-auto"/>
             </div>
             <div className="col-span-1 sm:col-span-2 grid grid-cols-1 sm:grid-cols-2 flex gap-4">
-                <div className={`content-box ${selectedEvent ? 'col-span-1' : 'hidden'} relative  max-sm:h-[400px]`}>
+                <div className={`content-box ${selectedEvent ? 'col-span-1' : 'hidden'} relative max-sm:h-[400px]`}>
                     <ViewEvent event={selectedEvent} onClose={() => setSelectedEvent(null)}
                                className="absolute inset-0 p-4 overflow-y-auto"/>
                 </div>
-                <div className={`content-box col-span-${selectedEvent ? '1' : '2'}`}>
-                    <CreateEvent mode={selectedEvent ? "edit" : "create"} event={selectedEvent} onEdit={() => setSelectedEvent(null)}/>
+                <div className={`content-box ${selectedEvent ? 'col-span-1' : 'sm:col-span-2'}`}>
+                    <CreateEvent mode={selectedEvent ? "edit" : "create"} event={selectedEvent}
+                                 onEdit={() => setSelectedEvent(null)}/>
                 </div>
             </div>
         </>
